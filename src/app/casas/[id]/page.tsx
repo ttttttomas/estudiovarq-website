843541030e7980d96f5eaeed43d83e3bbccb5df2
tomas <totobarajas124@gmail.com>
2025-06-17 14:28:15 -0300

    fixed id page

--- src/app/casas/[id]/page.tsx
+++ src/app/casas/[id]/page.tsx
@@ -21,19 +21,12 @@ export default function CasaIDpage() {
   }, [id]);
 
   return (
-<<<<<<< HEAD
     <main className="mx-5 mt-52 mb-10 md:mx-30">
+      Add commentMore actions
       <section className="my-30 flex flex-col items-center justify-center gap-10 md:flex-row">
         <div>
           <h1 className="text-primary text-center text-5xl font-bold">{house && house.title}</h1>
           <small className="text-gray-600">{house && house.houseType}</small>
-=======
-    <main className="mx-5 mt-52 md:mx-30">
-      <section className="my-30 flex flex-col justify-center gap-10 md:flex-row">
-        <div className="flex flex-col items-start justify-center gap-2">
-          <h1 className="text-primary text-center text-5xl font-bold">{house && house.title}</h1>
-          <small className="text-center text-gray-600">{house && house.houseType}</small>
->>>>>>> 32cbb3ff1125ef147edfe6c09fbcfb68906c6864
         </div>
         <p className="text-3xl">Diseñamos y construimos las mejores soluciones.</p>
       </section>
