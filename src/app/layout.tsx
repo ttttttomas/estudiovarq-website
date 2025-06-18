1fc33fb97a66fc049d5e328d9912bf2db4170b17
Tomas <totobarajas124@gmail.com>
2025-06-18 15:13:10 -0300

    add tags iframe google manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default async function RootLayout({children}: {children: React.ReactNode}
   return (
     <html lang="es">
       <Head>
-        {/* Google Analytics Script */}
+        {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-BTBZZ8PV52" />
         <script
           dangerouslySetInnerHTML={{
@@ -41,7 +41,7 @@ export default async function RootLayout({children}: {children: React.ReactNode}
           }}
         />
 
-        {/* Hotjar Script */}
+        {/* Hotjar */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -53,6 +53,24 @@ export default async function RootLayout({children}: {children: React.ReactNode}
                 r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
                 a.appendChild(r);
               })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),
+                    dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5JRV935S');
             `,
           }}
         />
@@ -122,6 +140,15 @@ export default async function RootLayout({children}: {children: React.ReactNode}
           </div>
         </footer>
       </body>
+      <noscript>
+        <iframe
+          height="0"
+          src="https://www.googletagmanager.com/ns.html?id=GTM-5JRV935S"
+          style={{display: "none", visibility: "hidden"}}
+          title="google analytics"
+          width="0"
+        />
+      </noscript>
     </html>
   );
 }
