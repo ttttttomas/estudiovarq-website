9befd332a4e26e909c2e5d9e0a4698f441b7da8f
Tomas <totobarajas124@gmail.com>
2025-06-18 14:56:15 -0300

    add tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,8 @@ import Header from "./components/Header";
 import "./globals.css";
 import {M2Provider} from "./context/M2Context";
 
+import Head from "next/head";
+
 export const encodeSans = Encode_Sans({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -25,6 +27,54 @@ export const metadata: Metadata = {
 export default async function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="es">
+      <Head>
+        {/* Google Analytics Script */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BTBZZ8PV52" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-BTBZZ8PV52');
+            `,
+          }}
+        />
+
+        {/* Hotjar Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:3874047,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
+
+        <meta content="Estudio Varq" name="author" />
+        <meta content="Estudio Varq" property="og:title" />
+        <meta content="Estudio Varq" property="og:site_name" />
+        <meta content="https://estudiovarq.com.ar" property="og:url" />
+        <meta content="https://estudiovarq.com.ar/logofooter.png" property="og:image" />
+        <meta content="https://estudiovarq.com.ar/logofooter.png" property="og:image:secure_url" />
+        <meta content="website" property="og:type" />
+        <meta content="summary_large_image" name="twitter:card" />
+        <meta content="@estudiovarq" name="twitter:site" />
+        <meta content="@estudiovarq" name="twitter:creator" />
+        <meta content="Estudio Varq" name="twitter:title" />
+        <meta
+          content="Realizamos reformas estratégicas para potenciar el inmueble y mejorar la calidad de vida de los que habitan tu casa. Cocinas. Baños. Livings. Habitaciones."
+          name="twitter:description"
+        />
+        <meta content="https://estudiovarq.com.ar/logofooter.png" name="twitter:image" />
+        <meta content="summary_large_image" name="twitter:card" />
+      </Head>
       <head>
         <link href="./Favicon.png" rel="icon" type="image/x-icon" />
         <meta content="width=device-width, initial-scale=1.0" name="viewport" />
