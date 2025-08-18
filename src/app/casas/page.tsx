d1eecc10da38c9e879274bfba776383abdef4323
Tomas <totobarajas124@gmail.com>
2025-08-17 23:03:32 -0300

    update houses imgs

--- src/app/casas/page.tsx
+++ src/app/casas/page.tsx
@@ -16,12 +16,12 @@ export default async function Casaspage() {
         Te mostramos algunas casas que podemos hacer!
       </h1>
       <section className="mx-5 my-20 flex flex-col items-center justify-center gap-5 md:mx-0 md:flex-row md:flex-wrap">
-        <HomeCard house={houses[0]} />
-        <HomeCard house={houses[2]} />
-        <HomeCard house={houses[4]} />
-        <HomeCard house={houses[7]} />
-        <HomeCard house={houses[3]} />
         <HomeCard house={houses[1]} />
+        <HomeCard house={houses[3]} />
+        <HomeCard house={houses[0]} />
+        <HomeCard house={houses[7]} />
+        <HomeCard house={houses[4]} />
+        <HomeCard house={houses[2]} />
         <HomeCard house={houses[6]} />
         <HomeCard house={houses[5]} />
         <HomeCard house={houses[8]} />
