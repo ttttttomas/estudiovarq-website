f7c7424fb9e13220006308d5ba32de74764aff10
Tomas <totobarajas124@gmail.com>
2025-09-10 01:48:20 -0300

    fix order houses

--- src/app/casas/page.tsx
+++ src/app/casas/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import axios from "axios";
 
 import HomeCard from "../components/HomeCard";
@@ -16,6 +17,8 @@ export default async function Casaspage() {
     slug: slugify(house.title),
   }));
 
+  console.log(houses);
+
   return (
     <main className="mt-40">
       <h1 className="text-primary mt-10 text-center text-3xl font-bold">
@@ -23,16 +26,15 @@ export default async function Casaspage() {
       </h1>
       <section className="mx-5 my-20 flex flex-col items-center justify-center gap-5 md:mx-0 md:flex-row md:flex-wrap">
         <HomeCard house={houses[8]} />
-        <HomeCard house={houses[4]} />
         <HomeCard house={houses[7]} />
-        <HomeCard house={houses[5]} />
-        <HomeCard house={houses[3]} />
-        <HomeCard house={houses[1]} />
         <HomeCard house={houses[2]} />
-        <HomeCard house={houses[9]} />
+        <HomeCard house={houses[1]} />
         <HomeCard house={houses[0]} />
-        <HomeCard house={houses[10]} />
+        <HomeCard house={houses[4]} />
+        <HomeCard house={houses[5]} />
+        <HomeCard house={houses[9]} />
         <HomeCard house={houses[6]} />
+        <HomeCard house={houses[3]} />
       </section>
       <section className="my-20">
         <WizardFormHouses />
