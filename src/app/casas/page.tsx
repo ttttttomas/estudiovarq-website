16de103f3fea98712af44e203b73b4f1ebadc9ba
Tomas <totobarajas124@gmail.com>
2025-09-17 00:40:58 -0300

    updated order houses

--- src/app/casas/page.tsx
+++ src/app/casas/page.tsx
@@ -15,22 +15,25 @@ export default async function Casaspage() {
     ...house,
     slug: slugify(house.title),
   }));
+
   return (
     <main className="mt-40">
       <h1 className="text-primary mt-10 text-center text-3xl font-bold">
         Te mostramos algunas casas que podemos hacer!
       </h1>
       <section className="mx-5 my-20 flex flex-col items-center justify-center gap-5 md:mx-0 md:flex-row md:flex-wrap">
-        <HomeCard house={houses[8]} />
-        <HomeCard house={houses[7]} />
+        <HomeCard house={houses[10]} />
+        <HomeCard house={houses[9]} />
+        <HomeCard house={houses[5]} />
         <HomeCard house={houses[2]} />
+        <HomeCard house={houses[3]} />
         <HomeCard house={houses[1]} />
         <HomeCard house={houses[0]} />
-        <HomeCard house={houses[4]} />
-        <HomeCard house={houses[5]} />
-        <HomeCard house={houses[9]} />
         <HomeCard house={houses[6]} />
-        <HomeCard house={houses[3]} />
+        <HomeCard house={houses[7]} />
+        <HomeCard house={houses[11]} />
+        <HomeCard house={houses[8]} />
+        <HomeCard house={houses[4]} />
       </section>
       <section className="my-20">
         <WizardFormHouses />
